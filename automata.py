832301e71360bd6b87b322f124480a1fdf543d03
GGreenwood <garrettagreenwood@gmail.com>
2014-08-05 20:39:00 -0500

    Fixed wrapping

--- automata.py
+++ automata.py
@@ -8,22 +8,27 @@ size = 150
 
 color = (125,125,150)
 
-ruleset = "00011110"
+ruleset = "00000010"[::-1]
+
+wrap = True
+
+delay = 1/60.0
+
 rules = []
-for x in reversed(ruleset):
+for x in ruleset:
     rules.append(x=="1")
 
-board = Board(size, color, False)
+board = Board(size, color, wrap)
 
 #for x in range(0,15):
 #    board.toggle(x*10)
-board.toggle(10)
+board.toggle(2)
 
 while True:
     client.put_pixels(board.output(), channel=0)
-    time.sleep(1/3.0)
+    time.sleep(delay)
 
-    new_board = Board(size, color)
+    new_board = Board(size, color, wrap)
     for x in range(0, size):
         new_board.get(x).alive = rules[board.score(x)]
     board = new_board
